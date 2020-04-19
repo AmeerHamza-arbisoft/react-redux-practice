3781980d6ad5d4f4e2917de580746d33f4e773b2
Ameer Hamza <ameer.hamza@arbisoft.com>
2020-04-20 01:36:23 +0500

    :wave: init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+
+import CoursesPage from "./components/courses/CoursesPage";
+import AboutPage from "./components/about/AboutPage";
+import HomePage from "./components/home/HomePage";
+import Header from "./components/shared/Header";
+import PageNotFound from "./PageNotFound";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
